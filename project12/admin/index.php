c2c0a048de177a5d50f47507879e55d88793ca6a
minh2561 <84631891+minh2561@users.noreply.github.com>
2021-10-28 00:07:47 +0700

    Update index.php

--- project12/admin/index.php
+++ project12/admin/index.php
@@ -1,48 +1,42 @@
 <?php
-    include '../index/header.php'
+    include '../index/header.php';
 ?>
     <div class="img_back">
         <div class="tieu_de">Thông tin môn học</div>
         <div class="row cot0">
             <div class="col-sm-2 cot cot1">
-                <div class="cot1_1">Môn học<hr></div>           
-                <div class="cot1">
-                    <div class="subject">
-                        <a href="#">Toán 1</a>
-                        <hr/>
-                    </div>
-                    <div class="subject">
-                        <a href="#">Toán 2</a>
-                        <hr/>
-                    </div>
-                    <div class="subject">
-                        <a href="#">Toán 3</a>
-                        <hr/>
-                    </div>
-                    <div class="subject">
-                        <a href="#">Triết</a>
-                        <hr/>
-                    </div>
-                    <div class="subject">
-                        <a href="#">Bóng đá</a>
-                        <hr/>
-                    </div>
-                </div>
-            </div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Môn học</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+            <?php
+                include './config.php';
+                $sql = "SELECT * FROM mon_hoc";
+                $result = mysqli_query($conn, $sql); 
+                if(mysqli_num_rows($result)>0) {
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo '<tr>';
+                            echo '<th scope="row"><a href="#">'.$row['mh_ten_mon'].'</a></th>';
+                        echo '</tr>';
+                    }
+                }
+            ?>  
+                    </tbody>
+                </table>
+            </div>        
         
-    
-    
-    
-    
-    
-    
-    
-    
-    
         
+        
+        
+            
+            
+            
+            
             <div class="col-sm-10 cot cot2">
-                <div class="cot2_1">Lớp học phần<hr></div>
-                
+                <div class="cot2_1">Lớp học phần<hr></div>                
             </div>
         </div>
     </div>
@@ -63,17 +57,15 @@
     
     
     
-    
-    
 
 
 
     
-    <h1 class="minh">
+    <!-- <h1 class="minh">
 	    <span data-z data-z-fade="true" data-z-layers="10" data-z-depth=".25em">🔥</span>
-    </h1>
+    </h1> -->
     <script src="https://bennettfeely.com/ztext/js/ztext.min.js"></script>
 
 <?php
-    include '../index/footer.php'
+    include '../index/footer.php';
 ?>
