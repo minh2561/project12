0888ba76f9d1766705047632dde91d0f919fca7f
Nguyễn Đức Lợi <90234593+loi02112001@users.noreply.github.com>
2021-10-28 04:25:21 +0700

    Update index.php

--- project12/sinhvien/index.php
+++ project12/sinhvien/index.php
@@ -6,24 +6,25 @@ include 'menu.php';
 <div class="wrapper">
     <h1>Đăng kí học</h1>
     <div>
-        <input type="text" placeholder="nhap mon hoc ..." onchange="handleGetName(this.value)">
+        <input type="text" placeholder="Nhập tên môn học" onchange="handleGetName(this.value)">
         <button class="btn btn-primary" id="btnSearch"><a id="hrefSearch">Tìm kiếm</a></button>
     </div>
-    <table class="tbl-full">
+    <table class="table">
+        <thead>
         <tr>
-            <th class ='px-3'>STT</th>
-            <th>Lớp học phần</th>
-            <th>Tên học phần</th>
-            <th>Trạng thái</th>
-            <th>Tổng sinh viên</th>
-            <th>Số sinh viên</th>
-            <th>Tên phòng</th>
-            <th>Tuần học</th>
-            <th>Gio học</th>
-            <th>Trạng thái đăng kí</th>
-            <th>Đăng kí</th>
-           
+            <th scope="col">STT</th>
+            <th scope="col">Lớp học phần</th>
+            <th scope="col">Tên học phần</th>
+            <th scope="col">Trạng thái</th>
+            <th scope="col">Tổng sinh viên</th>
+            <th scope="col">Số sinh viên</th>
+            <th scope="col">Tên phòng</th>
+            <th scope="col">Tuần học</th>
+            <th scope="col">Gio học</th>
+            <th scope="col">Trạng thái đăng kí</th>
+            <th scope="col">Đăng kí</th>   
         </tr>
+        </thead>
         <?php
         include '../config.php';
         $sql = "SELECT * FROM dang_ki_tin_chi";
@@ -31,6 +32,7 @@ include 'menu.php';
         if(mysqli_num_rows($res)>0){
            
             while($row=mysqli_fetch_assoc($res)){
+                echo '<tbody>';
                 echo '<tr>';
                 echo '<td>'.$row['lop_id'].'</td>';
                 echo '<td>'.$row['lop_hoc_phan'].'</td>';
@@ -45,6 +47,7 @@ include 'menu.php';
                 echo '<td><a href ="result.php?id='.$row['lop_id'].'" ><i class="fas fa-check-circle text-success"></i></a></td>'; 
                 
                 echo '</tr>';
+                echo '</tbody>';
               
             }
         }
