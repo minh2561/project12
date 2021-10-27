7a02ecf17541ea2545e2416f48a93d1d7f815095
minh2561 <84631891+minh2561@users.noreply.github.com>
2021-10-27 23:42:39 +0700

    Add files via upload
    admin

---
+++ project12/admin/index.php
@@ -0,0 +1,79 @@
+<?php
+    include '../index/header.php'
+?>
+    <div class="img_back">
+        <div class="tieu_de">Thông tin môn học</div>
+        <div class="row cot0">
+            <div class="col-sm-2 cot cot1">
+                <div class="cot1_1">Môn học<hr></div>           
+                <div class="cot1">
+                    <div class="subject">
+                        <a href="#">Toán 1</a>
+                        <hr/>
+                    </div>
+                    <div class="subject">
+                        <a href="#">Toán 2</a>
+                        <hr/>
+                    </div>
+                    <div class="subject">
+                        <a href="#">Toán 3</a>
+                        <hr/>
+                    </div>
+                    <div class="subject">
+                        <a href="#">Triết</a>
+                        <hr/>
+                    </div>
+                    <div class="subject">
+                        <a href="#">Bóng đá</a>
+                        <hr/>
+                    </div>
+                </div>
+            </div>
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+            <div class="col-sm-10 cot cot2">
+                <div class="cot2_1">Lớp học phần<hr></div>
+                
+            </div>
+        </div>
+    </div>
+    <div class="end"></div>
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+    
+    <h1 class="minh">
+	    <span data-z data-z-fade="true" data-z-layers="10" data-z-depth=".25em">🔥</span>
+    </h1>
+    <script src="https://bennettfeely.com/ztext/js/ztext.min.js"></script>
+
+<?php
+    include '../index/footer.php'
+?>
