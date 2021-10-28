c95fa392413515f5ef6be6f990308119db3388a4
ManhPhamGitHub <90790565+ManhPhamGitHub@users.noreply.github.com>
2021-10-28 20:37:09 +0700

    update indexx_admin

--- project12/index/index_admin.php
+++ project12/index/index_admin.php
@@ -1,5 +1,5 @@
 <?php
-    include '../index/header.php';
+    include './header.php';
 ?>
     <div class="back">
         <div class="tieude">
@@ -13,55 +13,24 @@
         </div>
         <div class="ground">
             <div class="sign">
-                <div class="in">     
-                    <form action="" method="POST">          
+                <div class="in">               
                     <div class="sign_in form-floating mb-3">
-                        <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
                         <label for="floatingInput">Email address</label>
                     </div>
                     <div class="sign_in form-floating">
-                        <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
                         <label for="floatingPassword">Password</label>
                     </div>
                     <div class="link">             
-                        <a href="./index_admin.php"><p>Bạn là quản trị viên</p></a>
-                        <a href="../index.php"><p>Bạn là sinh vien</p></a>
-                        <input type="submit" name="submit">
+                        <a href="../index.php"><p>Bạn là sinh viên</p></a>
+                        <a href="./index_gv.php"><p>Bạn là giảng viên</p></a>
+                        <button type="submit" class="button btn btn-outline-primary">Đăng nhập</button>
                     </div>
-                    </form>
                 </div>
             </div>
-            
-        </div>      
-                <?php 
-                if(isset($_SESSION['login']))
-                {
-                    echo $_SESSION['login'];
-                    unset($_SESSION['login']);
-                }
-                ?>
+        </div>     
     </div>
-
-
-<?php 
-if(isset($_POST['submit'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM giao_vien WHERE gv_email='$email' AND gv_password='$password'";
-    $res = mysqli_query($conn, $sql);
-    $count = mysqli_num_rows($res);
-    if($count==1)
-        {   
-            $row = mysqli_fetch_assoc($res);
-            $id =  $row['gv_id'];
-            $_SESSION['giao_vien'] = $id;
-            header('location:'.SITEURL.'giaovien/index.php');
-        }else{
-            $_SESSION['login'] = "<div class='error text-center'>Username or Password did not match.</div>";
-        }
-}
-?>
-
 <?php
-    include 'footer.php';
+    include './footer.php';
 ?>
