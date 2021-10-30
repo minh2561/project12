d3595768ffd7f5ab0c9b7a6c586a621c07c68b61
Nguyễn Đức Lợi <90234593+loi02112001@users.noreply.github.com>
2021-10-30 10:13:24 +0700

    Update giaovien/index.php

--- project12/giaovien/index.php
+++ project12/giaovien/index.php
@@ -1,17 +1,35 @@
 <?php
 include '../index/header.php';
-include 'menu.php';
-include 'check_login_gv.php'
+
+include 'check_login_gv.php';
 ?>
-<div class="main-content">
-<div class="wrapper">
-    <h1>Môn học</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-danger">
+    <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                <li class="nav-item ">
+                    <a class="nav-link" href="index.php"><i class="far fa-calendar-alt me-1"></i>Môn học</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="../index/logout.php"><i class="fas fa-sign-out-alt me-1"></i>Đăng xuất</a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+<div class="main-content bg-light" style="height: 500px;">
+    <div class="container">
+    <h3 class="text-center py-4">Môn được phân công</h3>
     
     <table class="table">
-        <thead>
+        <thead class = "bg-primary">
         <tr>
             <th scope="col">STT</th>
-            <th scope="col">Lớp học phần</th>
             <th scope="col">Tên học phần</th>
             <th scope="col">Tổng sinh viên</th>
             <th scope="col">Số sinh viên</th>
@@ -31,14 +49,14 @@ include 'check_login_gv.php'
                 echo '<tbody>';
                 echo '<tr>';
                 echo '<td>'.$i++.'</td>';
-                echo '<td>'.$row['lop_hoc_phan'].'</td>';
+                
                 echo '<td>'.$row['lop_ten_hoc_phan'].'</td>';
                 echo '<td>'.$row['lop_max_sv'].'</td>';
                 echo '<td>'.$row['lop_current_sv'].'</td>';
                 echo '<td>'.$row['lop_ten_phong'].'</td>';
                 echo '<td>'.$row['lop_tuan_hoc'].'</td>'; 
                 echo '<td>'.$row['lop_gio_hoc'].'</td>';
-                echo '<td><a href ="edit.php?id='.$row['lop_id'].'" ><i class="fas fa-edit"></i></a></td>'; 
+                echo '<td><a href ="edit.php?id='.$row['lop_id'].'" class = "text-primary"><i class="fas fa-list"></i></a></td>'; 
                 
                 echo '</tr>';
                 echo '</tbody>';
@@ -68,5 +86,6 @@ $(document).ready(async function(){
 
 </script>
 <?php
-include '../index/footer.php'
+include '../index/footer.php';
 ?>
+<div class="end"></div>
