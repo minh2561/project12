257f7ddf9c5ad870b65915dd704d5d0895a4de8d
ManhPhamGitHub <90790565+ManhPhamGitHub@users.noreply.github.com>
2021-10-30 19:41:01 +0700

    get gv_ten

--- project12/admin/getClass.php
+++ project12/admin/getClass.php
@@ -1,7 +1,7 @@
 <?php 
 include('../index/config.php');
 $mh_id = $_GET['mh_id'];
-$sql = "SELECT * FROM dang_ki_tin_chi WHERE mh_id='$mh_id'";
+$sql = "SELECT * FROM dang_ki_tin_chi INNER JOIN giao_vien ON dang_ki_tin_chi.gv_id = giao_vien.gv_id WHERE dang_ki_tin_chi.mh_id='$mh_id'";
         $res = mysqli_query($conn,$sql);
         $count = mysqli_num_rows($res);
         if($count = 0){
