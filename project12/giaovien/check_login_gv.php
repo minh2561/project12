a06c3c45a3a0ecf4ac338a726a8826b82fcb32b4
minh2561 <84631891+minh2561@users.noreply.github.com>
2021-11-04 03:49:51 +0700

    Delete project12 directory

--- project12/giaovien/check_login_gv.php
+++
@@ -1,9 +0,0 @@
-<?php 
-   
-    if(!isset($_SESSION['giao_vien'])) 
-    {
-        $_SESSION['no-login-message'] = "<div class='error text-center'>Please login to access </div>";
-        header('location:'.SITEURL.'index/index_gv.php');
-    }
-
-    ?>
--- project12/index/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    include('../index/config.php');
-    session_destroy(); //Unsets $_SESSION['user']
-
-    //2. REdirect to Login Page
-    header('location:'.SITEURL.'index.php');
-?>
